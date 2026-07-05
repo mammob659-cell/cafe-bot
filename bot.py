a12240da5e5458a1304eae6b3647ebe9bb6b7329
mammob659-cell <mammob659@gmail.com>
2026-07-05 10:29:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
 
+import os
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('مرحباً! البوت شغال بنجاح 🔥')
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot is running...")
+    app.run_polling()
